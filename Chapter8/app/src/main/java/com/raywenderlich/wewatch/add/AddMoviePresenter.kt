4bc04cb1341fe7f018319684befd964e1dd17c1f
harvathsteven <harvathsteven@gmail.com>
2021-03-21 17:31:10 -0700

    completing chapter 8

---
+++ Chapter8/app/src/main/java/com/raywenderlich/wewatch/add/AddMoviePresenter.kt
@@ -0,0 +1,21 @@
+package com.raywenderlich.wewatch.add
+
+import com.raywenderlich.wewatch.model.LocalDataSource
+import com.raywenderlich.wewatch.model.Movie
+
+class AddMoviePresenter(
+    private var viewInterface: AddMovieContract.ViewInterface,
+    private var dataSource: LocalDataSource
+) : AddMovieContract.PresenterInterface {
+
+    override fun addMovie(title: String, releaseDate: String, posterPath: String) {
+        if (title.isEmpty()) {
+            viewInterface.displayError("Movie title cannot be empty")
+        } else {
+            val movie = Movie(title, releaseDate, posterPath)
+            dataSource.insert(movie)
+            viewInterface.returnToMain()
+        }
+    }
+
+}
---
+++ Chapter8/app/src/main/java/com/raywenderlich/wewatch/main/MainContract.kt
@@ -0,0 +1,18 @@
+package com.raywenderlich.wewatch.main
+
+import com.raywenderlich.wewatch.model.Movie
+
+class MainContract {
+    interface PresenterInterface {
+        fun getMyMoviesList()
+        fun stop()
+        fun onDeleteTapped(selectedMovies: HashSet<*>)
+    }
+
+    interface ViewInterface {
+        fun displayMovies(movieList: List<Movie>)
+        fun displayNoMovies()
+        fun displayMessage(message: String)
+        fun displayError(message: String)
+    }
+}
